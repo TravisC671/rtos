0ea0859f7dbd124f6c1c4f4fcbdc363966660285
Calhoun <you@example.com>
2026-02-20 16:02:12 -0700

    starting doom port

---
+++ RTOS_Doom/include/doomgeneric_freertos.h
@@ -0,0 +1,12 @@
+#ifndef DOOMGENERIC_FREERTOS_H
+#define DOOMGENERIC_FREERTOS_H
+
+#define CMAP256
+
+// ! Change these
+#define DOOMGENERIC_RESX 320
+#define DOOMGENERIC_RESY 200
+
+// removed allegro audio files
+
+#endif // DOOMGENERIC_FREERTOS_H
